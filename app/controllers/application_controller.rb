323340388f452cac2a59626af67e4e51ccbdbc98
Eric Reale <ecr5083@gmail.com>
2019-11-22 19:51:40 +0800

    still no solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_devise, if: :devise_controller?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_action :configure_devise, if: :devise_controller?
 
   private
 
   def configure_devise
-    devise_parameter_sanitizer.permit(:user_update, keys: [ :first_name, :last_name, :username, images: [] ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :first_name, :last_name, :username, images: [] ])
   end
 end
